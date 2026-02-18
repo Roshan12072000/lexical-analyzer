30a65ee378b22100817720bd2f7326888328e891
Roshan12072000 <roshanjameer397@gmail.com>
2026-02-18 18:04:24 +0530

    testing erorr test.c

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main(){
 
-    float num = 12.34,a = 0777;
+    float num = 12.34,a = 0787;
     //int num1;
  
     char arr[] = {'A','B','C','D','\0'};
@@ -10,5 +10,6 @@ int main(){
     // scanf("%d",&num);
     for ( int i = 0; i < 2; i++)
     printf("The value of num is %d\n",a);
+    if(1){
     return 0;
 }
